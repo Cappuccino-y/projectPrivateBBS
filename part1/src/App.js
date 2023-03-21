c9d929af2115d72e8ccdb1922fe9c27137a8bc9c
幽幽的Yy <472860802@qq.com>
2023-03-21 21:20:12 +0800

    some fix

--- part1/src/App.js
+++ part1/src/App.js
@@ -15,13 +15,12 @@ const App = () => {
     setValue(newValue)
   }
 
-  return (
-    <div>
+  return <div>
       <Display value={value} />
       <Button handleClick={() => setToValue(1000)} text="thousand" />
       <Button handleClick={() => setToValue(0)} text="reset" />
       <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
-  )
+
 }
 export default App
