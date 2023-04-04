023342a6acb8249784b474423df859e4c28d189a
幽幽的Yy <472860802@qq.com>
2023-04-04 13:53:45 +0800

    initial change

--- part2/phonebook/src/components/PersonsShow.js
+++ part2/phonebook/src/components/PersonsShow.js
@@ -1,5 +1,5 @@
 const PersonsShow = ({personsShow, persondelete}) => <>{personsShow.map(person =>
-    <p key={person.number}>
+    <p key={person.number} className="person">
         {person.name} {person.number}
         <button onClick={() => persondelete(person.id)}>delete</button>
     </p>
