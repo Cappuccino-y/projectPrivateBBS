bbb91b7a7892b2f7d9611b169d5540ce1d411255
yydyy77 <472860802>
2023-04-20 17:00:34 +0800

    security

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,27 +9,26 @@ const footerStyle = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  backgroundColor: '#f8f8f8',
+  backgroundColor: '#ffffff',
   padding: '20px',
   position: 'fixed', // Add this line
   width: '100%', // Add this line
   bottom: '0', // Add this line
 };
+  const [linkColorICP, setLinkColorICP] = useState('#133');
+  const [linkColorPolice, setLinkColorPolice] = useState('#133');
 
+  const hoverColor = '#007bff';
   const linkStyle = {
-    color: '#333',
     textDecoration: 'none',
-    margin: '0 10px',
-  };
-
-  const hoverStyle = {
-    color: '#007bff',
+    fontSize:'12px',
+    margin: '0 10px'
   };
 
   return (
     <div style={footerStyle}>
-      <a href="https://beian.miit.gov.cn/"target="_blank" style={linkStyle} onMouseOver={() => {linkStyle.color = hoverStyle.color}} onMouseOut={() => {linkStyle.color = '#333'}}>浙ICP备2023009285号</a>
-      <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602013040"target="_blank" style={linkStyle} onMouseOver={() => {linkStyle.color = hoverStyle.color}} onMouseOut={() => {linkStyle.color = '#333'}}>浙公网安备 33010602013040号</a>
+      <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602013040" target="_blank" style={{ ...linkStyle, color: linkColorPolice}} onMouseOver={() => {setLinkColorPolice(hoverColor)}} onMouseOut={() => setLinkColorPolice('#333')}>浙公网安备 33010602013040号</a>
+      <a href="https://beian.miit.gov.cn/" target="_blank" style={{ ...linkStyle, color: linkColorICP }} onMouseOver={() => {setLinkColorICP(hoverColor)}} onMouseOut={() => setLinkColorICP('#333')}>浙ICP备2023009285号</a>
     </div>)
 }
 
--- part3/mongo.js
+++ part3/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
+if (process.argv.length != 3&&process.argv.length !=5) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }
@@ -8,9 +8,25 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://vicekrapperking:${password}@dataweb.rf3bc3x.mongodb.net/?retryWrites=true&w=majority`
-
+  `mongodb+srv://vicekrapperking:${password}@altasmongodb.tdby2dm.mongodb.net/noteData?retryWrites=true&w=majority`
 mongoose.connect(url)
+
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Number= mongoose.model('Number', noteSchema)
+
+if (process.argv.length==3){
+  console.log("phonebook:")
+  result.forEach(note => {
+    console.log(note)
+  })
+}
+
 
 const noteSchema = new mongoose.Schema({
   content: String,
@@ -21,7 +37,7 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'HTML is Easy',
+  content: 'HTML is Easy2',
   date: new Date(),
   important: true,
 })
@@ -29,4 +45,11 @@ const note = new Note({
 note.save().then(result => {
   console.log('note saved!')
   mongoose.connection.close()
+})
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+  process.exit(0)
 })
